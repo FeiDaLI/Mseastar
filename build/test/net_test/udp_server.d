93be8c96ce438512ed914743a27eb2e60405139d
FeiDaLI <1607741781@qq.com>
2025-05-15 21:41:02 +0800

    udp res

---
+++ build/test/net_test/udp_server.d
@@ -0,0 +1,14 @@
+build/test/net_test/udp_server.o: test/net_test/udp_server.cc \
+ test/net_test/../../include/app/app-template.hh \
+ test/net_test/../../include/app/../future/future_all10.hh \
+ test/net_test/../../include/app/../future/../task/task.hh \
+ test/net_test/../../include/app/../future/../util/shared_ptr.hh \
+ test/net_test/../../include/app/../future/do_with.hh \
+ test/net_test/../../include/app/../future/../resource/resource.hh \
+ test/net_test/../../include/app/../future/../util/align.hh \
+ test/net_test/../../include/app/../future/../util/bool_class.hh \
+ test/net_test/../../include/app/../future/../util/spinlock.hh \
+ test/net_test/../../include/app/../future/../util/unaligned.hh \
+ test/net_test/../../include/app/../future/../util/bitops.hh \
+ test/net_test/../../include/app/../future/../fd/posix.hh \
+ test/net_test/../../include/app/../future/../util/backtrace.hh
